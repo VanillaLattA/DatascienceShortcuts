f6b0db38043c888934ea2a3825d59db80e6c59f1
Bhargo <77913519+VanillaLattA@users.noreply.github.com>
2022-07-28 23:26:32 -0700

    finished and tested

--- stackClass.py
+++ stackClass.py
@@ -2,6 +2,7 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 import csv
+import math as m
 
 class stack():
     def __init__(self):
@@ -15,10 +16,12 @@ class stack():
     def peek(self):
         print(self.items[0])
     def printReverse(self):
-        #TODO print the stack in reverse
+        print(self.items)
     def peekMiddle(self):
-        #TODO implement this 
-
+        halfLength= m.floor(len(self.items)/2)
+        print(self.items[halfLength])
+        
+        
 
 testStack=stack() #Tests to see it works
 testStack.push(1)
@@ -29,3 +32,8 @@ testStack.pop()
 testStack.pop()
 testStack.printStack()
 testStack.peek()
+testStack.push("hello")
+testStack.push(133)
+testStack.printStack()
+testStack.peekMiddle()
+
