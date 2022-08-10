7fdc288ee60b2ef8adfc6b148958b70dce5a1076
Bhargo <77913519+VanillaLattA@users.noreply.github.com>
2022-08-10 14:16:09 -0700

    File writing features
    TODO: file clearing features

--- FileWriteIO.py
+++ FileWriteIO.py
@@ -6,12 +6,18 @@ import collections
 #a file with the delimeters being , and then having a class that does things with the file 
 #like giving word count, character count, and letter count for each letter
 
+#get the number of data sets that are being used in the file 
+try:
+    textEntry= int(input("How many data sets do you want to read into the file? "))
+except ValueError:
+    print("The value that you entered cannot be processed")
+    
+#open the file 
+fileOpen= open("testFile.txt", 'r+') #this will be cleared everytime per the first line
 
-textEntry= int(input("How many data sets do you want to read into the file? "))
-fileOpen= open("UserDataEnterBasics.txt") #this will be cleared everytime 
 for i in range(textEntry):
     nameOfEntry= input("What is the name of the person? ")
-    ageOfEntry= int(input("What is their age? "))
-    grossIncomeOfEntry= int(input("What is their gross income? "))
-    finalString = str(nameOfEntry + ageOfEntry + grossIncomeOfEntry)
-    print(finalString)
+    ageOfEntry= input("What is their age? ")
+    grossIncomeOfEntry= input("What is their gross income? ") 
+    finalString = str(nameOfEntry + ";" + ageOfEntry + ";"+ grossIncomeOfEntry)
+    fileOpen.write(finalString)
