05dd2e0fcd9ba85c043954236bfecd87cba196a0
Bhargo <77913519+VanillaLattA@users.noreply.github.com>
2022-08-09 21:32:34 -0700

    Maps letters and numbers in python

---
+++ FileIOMap.py
@@ -0,0 +1,15 @@
+import math
+import numpy as np
+import collections 
+
+letter_count={'a': 0, 'c': 0, 'b': 0, 'e': 0, 'd': 0, 'g': 0, 'f': 0, 'i': 0, 'h': 0, 'k': 0,
+ 'j': 0, 'm': 0, 'l': 0, 'o': 0, 'n': 0, 'q': 0, 'p': 0, 's': 0, 'r': 0, 'u': 0, 
+'t': 0, 'w': 0, 'v': 0, 'y': 0, 'x': 0, 'z': 0, '1': 0, '0': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0}
+
+#open the file 
+openFile= open("sampletext.txt")
+#read all the lines and not the text
+allText= openFile.read()
+#print line by line
+for line in allText:
+    print(line)
